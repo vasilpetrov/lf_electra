5db7e8edaa186426120c6e4e64fe472eeb2b0381
jareckib <116510448+jareckib@users.noreply.github.com>
2023-08-03 16:08:25 +0100

    Update lf_electra.lua
    description changed

--- lf_electra.lua
+++ lf_electra.lua
@@ -9,11 +9,11 @@ version = "               v1.1.14  01/08/2023 made by Jarek Barwinski "
 desc = [[
 
    Cloning new ELECTRA tags or EM410x to T5577 tag. This script changes 
-   block 0. Additional data is written to block 3 and 4. The last 
-   ELECTRA ID can be accessed through the option ---> "-c". For copy 
-   directly from the original ELECTRA tag, ---> option "-e". For copy 
-   from input, EM410X ID ---> option "-s". Next option for cloning simple 
-   EM4102 ---> option "-m". If press  <enter> it,  which writes an ID. 
+   block 0 (Only for new ELECTRA tag). Additional data is written to block 3 
+   and 4. The last ELECTRA ID can be accessed through the option ---> "-c". 
+   For copy directly from the original ELECTRA tag, ---> option "-e". 
+   For copy from input, EM410X ID ---> option "-s". Next option for cloning 
+   simple EM4102 ---> option "-m". If press  <enter> it,  which writes an ID. 
    If press <n> ---> exit the script.
 ]]
 example = [[
